4cac4983b325f44848fd37499a86b79218c5295c
Vladimir <haywaster3@gmail.com>
2021-01-25 00:15:42 +0700

    second task

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 
 const numberOfFilms = prompt("Сколько фильмов вы посмотрели?", "");
 console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movie: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+console.log(personalMovieDB.count);
