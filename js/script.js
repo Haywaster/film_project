79eb6e07e3861fdc863dbf50de2afbe781f02c5c
Vladimir <haywaster3@gmail.com>
2021-01-25 01:00:03 +0700

    third task

--- js/script.js
+++ js/script.js
@@ -5,8 +5,18 @@ console.log(numberOfFilms);
 
 const personalMovieDB = {
     count: numberOfFilms,
-    movie: {},
+    movies: {},
     actors: {},
     genres: [],
     privat: false
 };
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько его оцените?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("На сколько его оцените?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB.movies);
